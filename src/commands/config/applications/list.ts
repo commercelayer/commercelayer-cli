bf88e249b56d6f788103dae4bf75b60f9a2facfe
Pierluigi Viti <pierluigi@commercelayer.io>
2021-01-20 12:39:14 +0100

    beautifyied list outpout

--- src/commands/config/applications/list.ts
+++ src/commands/config/applications/list.ts
@@ -1,12 +1,38 @@
 import fs from 'fs'
+import Table from 'cli-table3'
+import { join } from 'path'
 
 type ApplicationsList = (path: string) => void
 
+type Application = {
+	organizationSlug: string,
+	endpoint: string,
+	scope: string
+}
+
 const applicationsList: ApplicationsList = (path) => {
   const directories = fs.readdirSync(path)
-  directories
+  const apps = directories
     .filter((dir) => dir !== 'default')
-    .map((dir) => console.log(`- ${dir}`))
+	.map((dir) => { /*console.log(`- ${dir}`);*/
+			const cfg = fs.readFileSync(join(path, dir, 'config.json'), { encoding: 'utf8' })
+			return JSON.parse(cfg.toString())
+		})
+	appTable(apps)
 }
+
+
+const appTable = (apps: Array<Application>) => {
+	
+	const table = new Table({ 
+		head: [ 'Application', 'Base URL', 'Scope' ]
+		//, colWidths: [30]
+	})
+	apps.forEach(app => { table.push([ app.organizationSlug, app.endpoint, app.scope ]) })
+
+	console.log(table.toString())
+
+}
+
 
 export default applicationsList
