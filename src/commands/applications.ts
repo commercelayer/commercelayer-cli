462b4cd3aaccb1dcba01bd6469e59a5d86b5e40c
Pierluigi Viti <pierluigi@commercelayer.io>
2021-04-29 17:25:31 +0200

    chore(applications): add organization header

--- src/commands/applications.ts
+++ src/commands/applications.ts
@@ -42,9 +42,10 @@ export default class Applications extends Command {
       {
         key: { header: 'APPLICATION (KEY)', minWidth: 20, get: row => chalk.blueBright(row.key) },
         // slug: { header: '  SLUG  ', get: row => `  ${row.slug}  ` },
-        name: { header: '  NAME  ', get: row => `  ${row.name}  ` },
-        baseUrl: { header: 'BASE URL', get: row => `${row.baseUrl ? `${row.baseUrl}  ` : ''}` },
-        mode: { header: 'MODE\n', get: row => `${(row.mode === 'live') ? chalk.greenBright(row.mode) : chalk.yellowBright(row.mode)}` },
+        name: { header: '  NAME', get: row => `  ${row.name}` },
+        organization: { header: '  ORGANIZATION  ', get: row => `  ${row.organization}  ` },
+        baseUrl: { header: 'BASE URL', get: row => `${row.baseUrl ? `${row.baseUrl}` : ''}` },
+        mode: { header: '  MODE\n', get: row => `  ${(row.mode === 'live') ? chalk.greenBright(row.mode) : chalk.yellowBright(row.mode)}` },
       },
       {
         printLine: this.log,
