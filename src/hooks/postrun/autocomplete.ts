e2e1002568b8134f803d0ee2f78becceb559316b
Pierluigi Viti <pierluigi@commercelayer.io>
2021-12-21 11:07:04 +0100

    Fix autocomplete on windows os

--- src/hooks/postrun/autocomplete.ts
+++ src/hooks/postrun/autocomplete.ts
@@ -4,13 +4,22 @@ import chalk from 'chalk'
 
 const hook: Hook<'postrun'> = async function (opts) {
 
+  // Windows OS not supported
+  if (opts.config.windows) return
+
   // Check if autocomplete plugin is installed
   if (opts.config.plugins.filter(p => p.name === '@oclif/plugin-autocomplete').length > 0) {
 
     // If a plugin is installed/uninstalled refresh autocomplete cache
     if (['plugins:install', 'plugins:uninstall'].includes(opts.Command.id)) {
-      const acCmd = opts.config.findCommand('autocomplete')
-      if (acCmd && (opts.argv[0] !== '@oclif/plugin-autocomplete')) await acCmd.load().run(['--refresh-cache'], opts.config)
+
+      try {
+        const acCmd = opts.config.findCommand('autocomplete')
+        if (acCmd && (opts.argv[0] !== '@oclif/plugin-autocomplete')) await acCmd.load().run(['--refresh-cache'], opts.config)
+      } catch (error: any) {
+        this.error(error.message)
+      }
+
     } else if ((opts.Command.id === 'autocomplete') && !opts.argv.includes('-r') && !opts.argv.includes('--refresh-cache')) {
       this.log(`${chalk.yellowBright('WARNING')}`)
       this.log(`-- Autocomplete feature currently works only using the extended command ${chalk.cyan(this.config.bin)} (not ${chalk.cyan('cl')} or ${chalk.cyan('clayer')}})`)
