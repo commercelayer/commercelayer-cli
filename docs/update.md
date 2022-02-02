0f46cecfca63af123761b65d85164a7235a10140
Pierluigi Viti <pierluigi@commercelayer.io>
2022-02-02 16:23:08 +0100

    chore(version): add version flag to update command

--- docs/update.md
+++ docs/update.md
@@ -11,10 +11,16 @@ Update Commerce Layer CLI.
 
 ```
 USAGE
-  $ commercelayer update
+  $ commercelayer update [-v <value>]
+
+FLAGS
+  -v, --version=<value>  Update CLI to a specific version or tag
 
 DESCRIPTION
   Update Commerce Layer CLI
+
+EXAMPLES
+  $ commercelayer update --version=<version-or-tag>
 ```
 
 _See code: [src/commands/update.ts](https://github.com/commercelayer/commercelayer-cli/blob/main/src/commands/update.ts)_
--- src/commands/update.ts
+++ src/commands/update.ts
@@ -1,4 +1,4 @@
-import { Command, CliUx as cliux } from '@oclif/core'
+import { Command, CliUx as cliux, Flags } from '@oclif/core'
 import { exec } from 'child_process'
 
 
@@ -6,20 +6,40 @@ export default class Update extends Command {
 
   static description = 'Update Commerce Layer CLI'
 
+  static examples = [
+    '<%= config.bin %> <%= command.id %> --version=<version-or-tag>',
+  ]
+
+  static flags = {
+    version: Flags.string({
+      char: 'v',
+      description: 'Update CLI to a specific version or tag',
+      required: false,
+    }),
+  }
+
 
   public async run(): Promise<void> {
+
+    const { flags } = await this.parse(Update)
+
+    const version = `@${flags.version}` || ''
 
     this.log()
     cliux.ux.action.start('Updating Commerce Layer CLI')
 
-    const cp = exec('npm install -g @commercelayer/cli')
+    const cp = exec(`npm install -g @commercelayer/cli${version}`)
 
     cp.on('exit', async () => {
+
       cliux.ux.action.stop()
+
       const verCmd = await this.config.findCommand('version')?.load()
       await verCmd?.run()
+
       // eslint-disable-next-line no-process-exit, unicorn/no-process-exit
       process.exit()
+
     })
 
   }
