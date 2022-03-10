c3f99994e2fa9d11df21cba5f040399c6a31b6ce
Pierluigi Viti <pierluigi@commercelayer.io>
2022-03-10 12:57:08 +0100

    fix(update): fix error handler

--- src/commands/cli/update.ts
+++ src/commands/cli/update.ts
@@ -30,19 +30,38 @@ export default class CliUpdate extends Command {
     this.log()
     cliux.ux.action.start('Updating Commerce Layer CLI')
 
-    const cp = exec(`npm install -g @commercelayer/cli${version}`)
 
-    cp.on('exit', async () => {
+    const cp = exec(`npm install -g @commercelayer/cli${version}`, async (error, _stdout, stderr) => {
 
-      cliux.ux.action.stop()
+      let errorMessage = ''
 
-      const verCmd = await this.config.findCommand('cli:version')?.load()
-      await verCmd?.run()
+      if (error) errorMessage = error.message
+      if (stderr && !stderr.includes('Reshimming asdf nodejs')) errorMessage = String(stderr)
 
-      // eslint-disable-next-line no-process-exit, unicorn/no-process-exit
-      process.exit()
+      if (errorMessage) {
+        cliux.ux.action.stop('failed')
+        this.log(errorMessage)
+        this.error('CLI update failed.')
+      } else {
+        cliux.ux.action.stop()
+        const verCmd = await this.config.findCommand('cli:version')?.load()
+        await verCmd?.run()
+      }
 
     })
+
+
+    cp.on('exit', async (code, signal) => {
+      if (code || signal) {
+        this.log()
+        this.log('Code: ' + code)
+        this.log('Signal: ' + signal)
+        this.log()
+        // eslint-disable-next-line no-process-exit, unicorn/no-process-exit
+        process.exit()
+      }
+    })
+
 
   }
 
