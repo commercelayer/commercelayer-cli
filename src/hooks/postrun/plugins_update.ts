a1b454653a4708fa513251984848666a4d60f497
Pierluigi Viti <pierluigi@commercelayer.io>
2023-06-27 11:56:36 +0200

    feat(plugins): show plugins version after update

---
+++ src/hooks/postrun/plugins_update.ts
@@ -0,0 +1,20 @@
+import { clColor } from '@commercelayer/cli-core'
+import type { Hook } from '@oclif/core'
+
+
+
+const hook: Hook<'postrun'> = async function (opts) {
+
+  if (opts.Command.id !== 'plugins:update') return
+
+  this.log(`\n${clColor.style.title('Current plugins version')}\n`)
+
+  const pluginsCmd = await this.config.findCommand('plugins')?.load()
+  await pluginsCmd?.run([])
+
+  this.log('')
+
+}
+
+
+export default hook
