9613963b95fc4805bbc98e1f8ac2cb4b54421b3f
Pierluigi Viti <pierluigi@commercelayer.io>
2024-02-06 09:51:23 +0100

    fix: fix shared resource types

--- src/patches/provisioning.ts
+++ src/patches/provisioning.ts
@@ -1,19 +1,34 @@
 
+import { clText } from "@commercelayer/cli-core"
 import { CommerceLayerProvisioningStatic } from "@commercelayer/provisioning-sdk"
+
+
+const provisioningCommands = ['retrieve', 'list', 'create', 'update', 'delete', 'get', 'relationship', 'fetch']
 
 
 export const checkProvisioningCommand = (argv: string[]): string[] => {
 
-	const provisioningResources = CommerceLayerProvisioningStatic.resources()
-
 	const cmdIdx = process.argv.findIndex(a => !a.startsWith('/'))
 	const cmd = process.argv[cmdIdx]
-	const res = process.argv.find(a => !a.startsWith('/') && (a !== cmd))
 
-	if (res && ['retrieve', 'list', 'create', 'update', 'delete', 'get', 'relationship', 'fetch'].includes(cmd) && provisioningResources.includes(res)) {
-		process.argv[cmdIdx] = `provisioning:${cmd}`
+	// Check provisioning command
+	if (provisioningCommands.includes(cmd)) {
+
+		const res = process.argv.find(a => !a.startsWith('/') && (a !== cmd))
+
+		// Check provisioning resource
+		if (res) {
+
+			const provisioningResources = [ ...CommerceLayerProvisioningStatic.resources() ]
+			provisioningResources.splice(provisioningResources.indexOf('organizations'), 1)
+			provisioningResources.splice(provisioningResources.indexOf('versions'), 1)
+
+			const provisioningResource = provisioningResources.includes(res) || (provisioningResources.includes(clText.pluralize(res)))
+			if (provisioningResource) process.argv[cmdIdx] = `provisioning:${cmd}`
+
+		}
+
 	}
-
 
 	return argv
 
