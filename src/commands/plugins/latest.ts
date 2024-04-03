3284d8793fb6edce60a4a14a1a77b25231307518
Pierluigi Viti <pierluigi@commercelayer.io>
2024-04-03 15:03:01 +0200

    feat: show latest plugins on beta channel

--- src/commands/plugins/latest.ts
+++ src/commands/plugins/latest.ts
@@ -1,4 +1,4 @@
-import { Args, Command, ux as cliux } from '@oclif/core'
+import { Args, Flags, Command, ux as cliux } from '@oclif/core'
 import { type PluginRelease, getAvailablePlugins, getPluginInfo } from './available'
 import { clColor } from '@commercelayer/cli-core'
 
@@ -11,13 +11,22 @@ export default class PluginsLatest extends Command {
     '$ cl plugins:latest resources'
   ]
 
+  static flags = {
+    beta: Flags.boolean({
+      char: 'b',
+      description: 'retrieve latest version from beta channel'
+    })
+  }
+
   static args = {
     plugin: Args.string({ description: 'the name of the plugin', required: false })
   }
 
   public async run(): Promise<string> {
 
-    const { args } = await this.parse(PluginsLatest)
+    const { args, flags } = await this.parse(PluginsLatest)
+
+    const beta = flags.beta || false
 
     const pluginInfo = getPluginInfo(args.plugin)
     if (!pluginInfo && args.plugin) this.error(`Plugin not available: ${clColor.msg.error(args.plugin)}`)
@@ -33,13 +42,14 @@ export default class PluginsLatest extends Command {
 
     this.log()
 
-    cliux.action.start('Fetching plugins')
+    cliux.action.start(`Fetching plugins${beta? ' [beta]' : ''}`)
     const output: string[] = []
     for (const plugin of plugins) {
 
       const pluginEntry = plugin.plugin
 
-      const latest = await fetch(`https://registry.npmjs.org/${pluginEntry}/latest`)
+      const channel = beta? 'beta' : 'latest'
+      const latest = await fetch(`https://registry.npmjs.org/${pluginEntry}/${channel}`)
         .then(async response => response.json())
         .catch(() => {
           this.error('Unable to fetch latest plugins, please retry later')
