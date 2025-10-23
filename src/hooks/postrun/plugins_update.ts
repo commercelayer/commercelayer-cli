47811f564247ae51436cf1ec246ddd0fc7805625
Pierluigi Viti <pierluigi@commercelayer.io>
2025-10-23 10:14:29 +0200

    feat: publish metrics plugin

--- src/hooks/postrun/plugins_update.ts
+++ src/hooks/postrun/plugins_update.ts
@@ -7,7 +7,7 @@ const hook: Hook<'postrun'> = async function (opts) {
 
   if (opts.Command.id !== 'plugins:update') return
 
-  this.log(`\n${clColor.style.title('Current installed plugins')}\n`)
+  this.log(`\n${clColor.style.title('Currently installed plugins')}\n`)
 
   const pluginsCmd = await this.config.findCommand('plugins')?.load()
   await pluginsCmd?.run([])
